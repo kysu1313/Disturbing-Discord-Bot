09bb9394ae88b8854b82dde22b9237640e032574
Kyle Supple <38538913+kysu1313@users.noreply.github.com>
2021-07-08 21:06:02 -0400

    First

---
+++ app.py
@@ -0,0 +1,121 @@
+import discord
+from discord.ext import commands, tasks
+from discord.ext.commands import Bot
+from cogs.parts.crypto_api import CryptoApi
+from cogs.parts.pokemon_api import PokeApi
+from dotenv import load_dotenv
+from cogs.commands import Commands
+import random
+import re
+import time
+import random
+import os
+import platform
+from distutils.command.config import config
+
+
+coins = ['AUR','BCC','BCH','BTC','DASH','DOGE','EOS','ETC','ETH','GRC','LTC','KOI','MZC','NANO','NEO','NMC','NXT','POT','PPC','TIT','USDC','USDT','VTC','XEM','XLM','XMR','XPM','XRP','XVG','ZEC']
+greetings = ['hi', 'hey', 'yo', 'hello', 'whats up', "what's up", 'yoo', 'yooo', 'sup', 'ayo', 'ayoo']
+times = {}
+description = '''None of your business, mkay'''
+ENABLED = True
+
+client = discord.Client()
+bot = Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name}")
+    print(f"Discord.py API version: {discord.__version__}")
+    print(f"Python version: {platform.python_version()}")
+    print(f"Running on: {platform.system()} {platform.release()} ({os.name})")
+    print("-------------------")
+
+
+@bot.event
+async def on_member_join(member):
+    server = member.server
+    fmt = 'Welcome {0.mention} to {1.name}!'
+    await member.send(server, fmt.format(member, server))
+
+@bot.event
+async def on_command_completion(ctx):
+    fullCommandName = ctx.command.qualified_name
+    split = fullCommandName.split(" ")
+    executedCommand = str(split[0])
+    print(
+        f"Executed {executedCommand} command in {ctx.guild.name} (ID: {ctx.message.guild.id}) by {ctx.message.author} (ID: {ctx.message.author.id})")
+
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f'cogs.{extension}')
+
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
+
+@bot.event
+async def on_message(message):
+    global ENABLED
+    if '!startbot' in message.content:
+        ENABLED = True
+    if ENABLED:
+        if message.author == bot.user:
+            return
+
+        msg = message.content.lower()
+
+        if '!stopbot' in msg:
+            ENABLED = False
+            return 
+
+        crypto = CryptoApi()
+        items = msg.split(' ')
+        for item in items:
+            if item.upper() in coins:
+                await message.channel.send('The current price of {} is ${}'.format(item.upper(), crypto.get_price(item.upper())))
+
+        if 'yay' in msg.split(' '):
+            await message.channel.send("🤩")
+
+        if '🔪' in msg:
+            await message.channel.send('🔫🙂')
+
+        if 'bitch' in msg.lower():
+            await message.channel.send("fuck you bitch")
+
+        if 'kys' in msg.split(' '):
+            await message.channel.send(':sob:')
+
+        if 'kms' in msg.split(' '):
+            await message.channel.send('🙂  Here, take this 🔫')
+
+        if any(word in msg.split(" ") for word in greetings):
+            await message.channel.send('hi 🙂')
+
+        poke = PokeApi()
+        if 'skills:' in msg.lower():
+            item = message.channel.content.split(":")[1].strip()
+            result = poke.get_skills(item)
+            if result is None:
+                await message.channel.send('No skills found 😢')
+            else:
+                await message.channel.send(', '.join(result))
+
+        await bot.process_commands(message)
+
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+if __name__ == '__main__':
+    ENABLED = True
+    load_dotenv()
+    TOKEN = os.getenv('discord-token')
+    #bot.add_cog(Commands(bot))
+    #bot.load_extension("parts.general")
+    #commands = Commands(bot)
+    bot.run(TOKEN)
+
+
---
+++ cogs/commands.py
@@ -0,0 +1,131 @@
+import discord
+from discord.ext import commands
+import os
+import random
+from dotenv import load_dotenv
+import threading
+import datetime
+
+class Commands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        load_dotenv()
+        self.bot_id = os.getenv('bot-id')
+
+    # Events
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'{self.bot.user.name} has connected to Discord!')
+
+    def checkTime(self):
+        # This function runs periodically every 1 second
+        threading.Timer(1, checkTime).start()
+
+        now = datetime.now()
+
+        current_time = now.strftime("%H:%M:%S")
+        print("Current Time =", current_time)
+
+        if(current_time == '02:11:00'):  # check if matches with the desired time
+            print('sending message')
+
+    # Commands
+    @commands.command(help='Simple check to verify bot is functioning properly')
+    async def ping(self, ctx):
+        await ctx.send('🏓 Pong!')
+
+    @commands.command(name='guess', help='Guess the number between 1 and 10')
+    async def guess(self, ctx):
+        await ctx.send('Guess a number between 1 to 10')
+
+        def guess_check(m):
+            return m.content.isdigit()
+        is_good = False
+        for i in range(0,3):
+            guess = await ctx.wait_for('message', check=guess_check, timeout=15)
+            answer = random.randint(1, 10)
+            if guess is None:
+                fmt = 'Sorry, you took too long. It was {}.'
+                await self.bot.send(fmt.format(answer))
+                return
+            if int(guess.content) == answer:
+                await ctx.send('You are right!')
+                is_good = True
+            else:
+                await ctx.send('WRONG, fucking idiot')
+        if not is_good:
+            await ctx.send('Sorry. It is actually {}.'.format(answer))
+
+    @commands.command(name="poll", help='Creates new poll with responses: 👍, 👎, 🤷')
+    async def poll(self, context, *, title):
+        """
+        Creates a new poll where members can vote
+        """
+        embed = discord.Embed(
+            title="A new poll has been created!",
+            description=f"{title}",
+            color=0x42F56C
+        )
+        embed.set_footer(
+            text=f"Poll created by: {context.message.author} • React to vote!"
+        )
+        embed_message = await context.send(embed=embed)
+        await embed_message.add_reaction("👍")
+        await embed_message.add_reaction("👎")
+        await embed_message.add_reaction("🤷")
+
+    @commands.command(name="invite", help='Sends you a private message you can use to invite the bot to another server')
+    async def invite(self, context):
+        """
+        Get the invite link of the bot to be able to invite it.
+        """
+        embed = discord.Embed(
+            description=f"Invite me by clicking [here](https://discordapp.com/oauth2/authorize?&client_id={self.bot_id}&scope=bot&permissions=470150263).",
+            color=0xD75BF4
+        )
+        try:
+            # To know what permissions to give to your bot, please see here: https://discordapi.com/permissions.html and remember to not give Administrator permissions.
+            await context.author.send(embed=embed)
+            await context.send("I sent you a private message!")
+        except discord.Forbidden:
+            await context.send(embed=embed)
+
+    @commands.command(name="8ball", help='!8ball <ask a question>')
+    async def eight_ball(self, context, *, question):
+        """
+        Ask any question to the bot.
+        """
+        answers = ['It is certain.', 'It is decidedly so.', 'You may rely on it.', 'Without a doubt.',
+                   'Yes - definitely.', 'As I see, yes.', 'Most likely.', 'Outlook good.', 'Yes.',
+                   'Signs point to yes.', 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.',
+                   'Cannot predict now.', 'Concentrate and ask again later.', 'Don\'t count on it.', 'My reply is no.',
+                   'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
+        embed = discord.Embed(
+            title="**My Answer:**",
+            description=f"{answers[random.randint(0, len(answers))]}",
+            color=0x42F56C
+        )
+        embed.set_footer(
+            text=f"The question was: {question}"
+        )
+        await context.send(embed=embed)
+
+    @commands.command(name='server', help='Returns server information')
+    async def fetchServerInfo(self, context):
+        guild = context.guild
+        
+        await context.send(f'Server Name: {guild.name}')
+        await context.send(f'Server Size: {len(guild.members)}')
+        await context.send(f'Server Name: {guild.owner.display_name}')
+
+    @commands.command(name='roll', help='!roll <# of dice> <# of sides>')
+    async def roll(self, ctx, number_of_dice: int, number_of_sides: int):
+        dice = [
+            str(random.choice(range(1, number_of_sides + 1)))
+            for _ in range(number_of_dice)
+        ]
+        await ctx.send(', '.join(dice) + ' 🎲')
+
+
+def setup(bot):
+    bot.add_cog(Commands(bot))
